c76afe9d1f2edee53ec0df165d5ee93a5965c2c6
nettk132 <nettk1322@gmail.com>
2023-10-04 12:37:38 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div >
+  <navbar_homepage />
+  <div >
+    <router-view />
+  </div>
+</div>
+</template>
+
+<script setup>
+import navbar_homepage from './components/navbar_homepage.vue';
+</script>
+
+<style lang="scss" scoped>
+.body {
+  background: rgb(255,255,255);
+background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(242,108,108,1) 100%);
+}
+</style>
